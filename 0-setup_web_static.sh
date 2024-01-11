62c9ddfe42a066df4df65b586c6a34ae29fa59e9
stevembui52 <stevembui52@gmail.com>
2024-01-10 22:23:49 -0800

    trial 4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,63 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Installs, configures, and starts the web server
 
-apt-get update
-apt-get install -y nginx
+SERVER_CONFIG="server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
+
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+
+	if (\$request_filename ~ redirect_me) {
+		rewrite ^ https://github.com/stevembui52 permanent;
+	}
+
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}"
+HOME_PAGE="<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+"
+# shellcheck disable=SC2230
+if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
+    apt-get update
+    apt-get -y install nginx
+fi
+mkdir -p /var/www/html /var/www/error
+chmod -R 755 /var/www
+echo 'Hello World!' > /var/www/html/index.html
+echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
+
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
+[ -d /data/web_static/current ] && rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $hostname;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://github.com/besthor;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+chown -hR ubuntu:ubuntu /data
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+if [ "$(pgrep -c nginx)" -le 0 ]; then
+	service nginx start
+else
+	service nginx restart
+fi

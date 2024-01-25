fc6057a96ca90953cc543896e289a4af17a3db2e
stevembui52 <stevembui52@gmail.com>
2024-01-25 05:20:20 -0800

    route 2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,8 +3,6 @@
 Your web application must be listening on 0.0.0.0, port 5000
 In Route /: display “Hello HBNB!”
 You must use the option strict_slashes=False in your route definition"""
-
-
 from flask import Flask
 app = Flask(__name__)
 
@@ -13,6 +11,12 @@ app = Flask(__name__)
 def hello_world():
     """ Display a custom String on main Route """
     return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display a custom message """
+    return "HBNB"
 
 
 if __name__ == '__main__':

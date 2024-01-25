3b9d0295245260b021fbb6b90b6f5109e99543ce
stevembui52 <stevembui52@gmail.com>
2024-01-25 05:57:29 -0800

    route python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Program that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+In Route /: display “python<text>”
+You must use the option strict_slashes=False in your route definition"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display a custom String on main Route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display a custom message """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Return string “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': "is_cool"})
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Function that receives a keyword argument and display a message
+    or a default value """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

c6eb89056b7d5dda86f8293e932195d121add14f
stevembui52 <stevembui52@gmail.com>
2024-01-25 05:59:09 -0800

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Program that starts a Flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+In Route /: display “python<text>”
+You must use the option strict_slashes=False in your route definition"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Display a custom String on main Route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display a custom message """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Return string “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': "is_cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Function that receives a keyword argument and display a message
+    or a default value """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """
+    Return “n is a number” only if `num` is an integer
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
